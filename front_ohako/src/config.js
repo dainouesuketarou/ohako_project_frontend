9477ad324ea08f47befec5abae6b6886a5685ba3
dainouesuketarou <147264374+dainouesuketarou@users.noreply.github.com>
2024-06-12 16:00:13 +0900

    API_BASE_URLをデバッグ用に変更しました

--- front_ohako/src/config.js
+++ front_ohako/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_BASE_URL: "http://127.0.0.1:8000/api/music"
+    API_BASE_URL: "https://backendohako-746be3b32e21.herokuapp.com/api/music"
   };
   
   export default config;
