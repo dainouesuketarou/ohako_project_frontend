c62df6af97cc2180669a5461f41224ad16e0989c
dainouesuketarou <147264374+dainouesuketarou@users.noreply.github.com>
2024-06-16 16:59:21 +0900

    Update config.js

--- front_ohako/src/config.js
+++ front_ohako/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_BASE_URL: "backendohako-746be3b32e21.herokuapp.com/api/music"
+    API_BASE_URL: "https://backendohako-746be3b32e21.herokuapp.com/api/music"
   };
   
   export default config;
